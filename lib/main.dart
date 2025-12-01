e20fefafc7fbe1a361a8bd6e76b02986c568fa3c
yong geon <gunnylove@gmail.com>
2025-12-02 01:38:23 +0700

    update

--- lib/main.dart
+++ lib/main.dart
@@ -44,7 +44,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Long-term Investment',
+      title: 'Time Capital',
       localizationsDelegates: const [
         AppLocalizations.delegate,
         GlobalMaterialLocalizations.delegate,
